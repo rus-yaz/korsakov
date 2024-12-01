0c7e4a921650c59f0285734796db89e433d245b8
x1z53 <x1z53@yandex.ru>
2024-12-01 19:14:57 +0300

    v0.6.44+1: Захардкоженный «Привет, мир!»

--- Ассембли/main.asm
+++ Ассембли/main.asm
@@ -15,18 +15,46 @@ include "lib/functions.asm"
 
 include "tests/const_buffers.asm"
 
+тестовый_файл db "привет, мир.корс", 0
+текст_функции db 'показать("привет, мир!")', 10, 0
+привет_мир db "привет, мир!", 0
+
+файловый_дескриптор rq 1
+содержимое rq 1
+тестовая_функция rq 1
+
 section "_start" executable
 _start:
   ; Аллокация кучи. Обязательна для любой работы с переменными
   allocate_heap
 
-  include "tests/heap.asm"
-  include "tests/file.asm"
-  include "tests/string.asm"
-  include "tests/list.asm"
-  include "tests/print.asm"
-  include "tests/exec.asm"
-  include "tests/functions.asm"
+  ;include "tests/heap.asm"
+  ;include "tests/file.asm"
+  ;include "tests/string.asm"
+  ;include "tests/list.asm"
+  ;include "tests/print.asm"
+  ;include "tests/exec.asm"
+  ;include "tests/functions.asm"
 
+  open_file тестовый_файл
+  mov [файловый_дескриптор], rax
+
+  read_file rax
+  mov [содержимое_файла], rax
+
+  close_file [файловый_дескриптор]
+
+  buffer_to_string текст_функции
+  mov [тестовая_функция], rax
+
+  ;print <[содержимое_файла], [тестовая_функция]>
+
+  is_equal [содержимое_файла], [тестовая_функция]
+  cmp rax, 1
+  je .continue
+    exit -1
+
+  .continue:
+  print привет_мир
 
   exit 0
