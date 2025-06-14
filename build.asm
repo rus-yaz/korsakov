45f1adb9ace92c69769638213ff73c9407917d45
x1z53 <x1z53@.>
2025-06-14 22:27:02 +0300

    v1.16.19-beta: Улучшение сборочной системы
    `build.asm` теперь принимает цели (одна за раз):
    - `korsakov` — генерация исполнителя
    - `tests`    — генерация запуска тестов

--- build.asm
+++ build.asm
@@ -11,8 +11,6 @@ list
 mov rbx, rax
 string "/bin/fasm"
 list_append_link rbx, rax
-string "korsakov.asm"
-list_append_link rbx, rax
 string "-m"
 list_append_link rbx, rax
 string "131072"
@@ -29,18 +27,43 @@ jne .no_debug
   list_append_link rbx, rax
 .no_debug:
 
+string "korsakov"
+
+mov rcx, rax
+list_include [ARGUMENTS], rcx
+boolean_value rax
+cmp rax, 1
+je .correct_target
+
+string "tests"
+mov rcx, rax
+list_include [ARGUMENTS], rcx
+boolean_value rax
+cmp rax, 1
+je .correct_target
+
+  raw_string "Некорректная цель"
+  error_raw rax
+  exit -1
+
+.correct_target:
+
+string ".asm"
+string_add_links rcx, rax
+list_append_link rbx, rax
+
 run rbx, [ENVIRONMENT_VARIABLES]
 
 list
 mov rbx, rax
 string "/bin/ld"
 list_append_link rbx, rax
-string "korsakov.o"
+string ".o"
+string_add_links rcx, rax
 list_append_link rbx, rax
 string "-o"
 list_append_link rbx, rax
-string "korsakov"
-list_append_link rbx, rax
+list_append_link rbx, rcx
 run rbx, [ENVIRONMENT_VARIABLES]
 
 exit 0
